2631a84b138b407d6d3455384c5199dd7313908c
Drane, Lance <dranelt@ornl.gov>
2021-01-19 23:45:27 -0500

    fix nginx util script
    Signed-off-by: Drane, Lance <dranelt@ornl.gov>

--- build-nginx-env.sh
+++ build-nginx-env.sh
@@ -12,7 +12,7 @@ fi
 
 rm -rf .ci/build || true
 # TODO sloppy handling for Docker users' volumes
-mv build .ci/build || sudo chown -R $USERNAME && mv build .ci/build
+mv build .ci/build || sudo chown -R $USERNAME . && mv build .ci/build
 docker build -f .ci/Dockerfile.nginx -t datastreams-fe-nginx .ci
 echo "You can now run 'docker run [OPTIONS] datastreams-fe-nginx:latest [COMMAND] [ARG...]'"
 echo "One flag you will probably want to include is '-p 80:80'"
