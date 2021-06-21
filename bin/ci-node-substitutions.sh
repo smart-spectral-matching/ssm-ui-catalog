aef6091b34df92c9d2dad390c051b10a9fa45f26
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-06-21 17:03:22 -0400

    Switches from master -> main

--- bin/ci-node-substitutions.sh
+++ bin/ci-node-substitutions.sh
@@ -7,4 +7,4 @@ set -e
 # environment variables
 sed -i "s~^[[:space:]]*REACT_APP_API_URL[[:space:]]*=.*~REACT_APP_API_URL=/api/~g" .env
 # robots.txt - disallow all crawlers if not in production
-[ "$CI_COMMIT_REF_NAME" == "master" ] || printf "User-agent: *\nDisallow: /\n" > public/robots.txt
+[ "$CI_COMMIT_REF_NAME" == "main" ] || printf "User-agent: *\nDisallow: /\n" > public/robots.txt
